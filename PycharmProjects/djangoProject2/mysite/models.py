08a2c35c6169865faa0352f613347c34d332582a
Maksym <maks.smolinets1978@gmail.com>
2024-10-16 23:38:33 +0300

    secound commit

--- PycharmProjects/djangoProject2/mysite/models.py
+++ PycharmProjects/djangoProject2/mysite/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Calculation(models.Model):
+    num1 = models.FloatField()
+    num2 = models.FloatField()
+    operation = models.CharField(max_length=10)
+    result = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.num1} {self.operation} {self.num2} = {self.result}"
--- PycharmProjects/djangoProject2/mysite/views.py
+++ PycharmProjects/djangoProject2/mysite/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Calculation
 
 
 def index(request):
     return render(request, 'mysite/index.html')
 
 
-def about(request):
-    return render(request, 'mysite/about.html')
+def ficha(request):
+    return render(request, 'mysite/ficha.html')
+
+
+def calculator(request):
+    result = None
+    if request.method == "POST":
+        if 'delete' in request.POST:
+            # Видалити обчислення
+            Calculation.objects.filter(id=request.POST.get('delete')).delete()
+        else:
+            num1 = float(request.POST.get('num1', 0))
+            num2 = float(request.POST.get('num2', 0))
+            operation = request.POST.get('operation')
+
+            if operation == 'add':
+                result = num1 + num2
+            elif operation == 'subtract':
+                result = num1 - num2
+            elif operation == 'multiply':
+                result = num1 * num2
+            elif operation == 'divide':
+                if num2 != 0:
+                    result = num1 / num2
+                else:
+                    result = 'Error: Division by zero'
+
+            # Зберігаємо результат в базу даних
+            if isinstance(result, (float, int)):  # Перевіряємо, чи результат числовий
+                Calculation.objects.create(num1=num1, num2=num2, operation=operation, result=result)
+
+    calculations = Calculation.objects.all()  # Отримуємо всі обчислення
+
+    return render(request, 'mysite/calculator.html', {'result': result, 'calculations': calculations})
+
